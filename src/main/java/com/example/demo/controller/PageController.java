113bccad87fbb63d52790813c44de2954cebca99
stasykobzeva <stasy.kobzeva@gmail.com>
2025-10-06 14:48:50 +0300

    updated

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
@@ -34,6 +35,11 @@ public class PageController {
         return "login"; // login.html
     }
 
+    @GetMapping("/book")
+    public String book() {
+        return "book"; // login.html
+    }
+
     @GetMapping("/register")
     public String registerPage() {
         return "register"; // register.html
@@ -43,5 +49,13 @@ public class PageController {
     public String getTask(@PathVariable int taskNumber, Model model) {
         model.addAttribute("taskNumber", taskNumber);
         return "task"; // шаблон task.html
+    }
+
+    @GetMapping("/info")
+    public String getInfo(@RequestParam("level") int level, Model model) {
+        // Передать уровень и содержимое в шаблон
+        model.addAttribute("level", level);
+        // Можно передать содержимое справочного материала, если есть
+        return "info"; // название шаблона info.html
     }
 }
