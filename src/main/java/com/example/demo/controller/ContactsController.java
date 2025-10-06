c2e48d4a262c537b11f4e99907f676f4bf95f786
annie-hilator <anna.rashoyan@gmail.com>
2025-10-06 21:06:10 +0300

    fixed some bugs

--- src/main/java/com/example/demo/controller/ContactsController.java
+++ src/main/java/com/example/demo/controller/ContactsController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +16,7 @@ public class ContactsController {
         List<Map<String, String>> contacts = Arrays.asList(
                 Map.of("name", "Рашоян Анна", "phone", "+7 123 456 78 90", "email", "anna@example.com"),
                 Map.of("name", "Кобзева Анастасия", "phone", "+7 987 654 32 10", "email", "anastasia@example.com"),
-                Map.of("name", "Александр Михайлов", "phone", "+7 555 666 77 88", "email", "alex@example.com")
-        );
+                Map.of("name", "Александр Михайлов", "phone", "+7 555 666 77 88", "email", "alex@example.com"));
         model.addAttribute("contacts", contacts);
         return "contacts"; // имя шаблона без расширения
     }
